47b850060f86942fb0edd030329763656900fed0
callmencah <cahyadi404notfound@gmail.com>
2023-02-17 20:44:06 +0700

    Template Automation Mobile Android Apps

---
+++ src/test/java/Runner/runAll.java
@@ -0,0 +1,14 @@
+package Runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/Feauters",
+        glue = "StepDefinitions",
+        plugin = "html:target/HTML_report.html"
+)
+public class runAll {
+}
